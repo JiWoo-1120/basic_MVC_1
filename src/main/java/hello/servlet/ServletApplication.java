f37eed2d6d08648e0271a0eb69a2633fb676eebd
JiWoo <didwldn97@naver.com>
2024-05-17 14:35:42 +0900

    뷰 리졸버 설정 prefix, suffix

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -12,4 +12,10 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	//application.properties에 설정해놓으면 스프링부트가 아래와 같은 코드의 기능을 자동으로 등록해준다.
+//	@Bean
+//	ViewResolver internalResourceViewResolver(){
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//
+//	}
 }
